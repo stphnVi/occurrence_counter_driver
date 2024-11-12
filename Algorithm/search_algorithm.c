f4d4605978b9a4a9285e99814986c1adae0e492a
Josue3108 <guille4567mo@gmail.com>
2024-11-12 05:14:01 -0600

    MPI integration

--- Algorithm/search_algorithm.c
+++ Algorithm/search_algorithm.c
@@ -1,8 +1,10 @@
 #include <stdio.h> 
 #include <stdlib.h> 
-#include <string.h> 
+#include <string.h>
+#include <mpi.h> 
 #include <ctype.h> 
 #include "search_algorithm.h"
+
  
 // Helper function to make a word lowercase 
 void toLowercase(char *str) { 
@@ -129,38 +131,81 @@ char **splitFileToStrings(const char *filename, int *numParts, int *outPartSizes
     fclose(file); 
     return partsArray; 
 } 
- 
-int main() { 
-    int numParts; 
 
+int main(int argc, char *argv[]) {
+    int rank, size;
+    MPI_Init(&argc, &argv);
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+    MPI_Comm_size(MPI_COMM_WORLD, &size);
+
+    int numParts;
     int *partSizes = (int *)malloc(MAX_PART_SIZE * sizeof(int));
-    if (!partSizes) { 
-        perror("Memory allocation for part sizes failed"); 
-        return 1; 
-    } 
- 
-    char **textParts = splitFileToStrings(FILENAME, &numParts, partSizes); 
-    if (!textParts) { 
-        free(partSizes); 
-        return 1; 
-    } 
- 
-    // Process each part and count word frequencies 
-    for (int i = 0; i < numParts; i++) { 
-        processPart(textParts[i]); 
-        free(textParts[i]);  // Free each part after processing 
-    } 
- 
-    free(textParts); 
-    free(partSizes); 
- 
-    // Find the most frequent word 
-    char mostFreqWord[MAX_WORD_LEN]; 
-    int maxCount; 
-    findMostFrequentWord(mostFreqWord, &maxCount); 
- 
-    printf("Most frequent word: '%s' (appears %d times)\n", mostFreqWord, maxCount); 
- 
-    cleanupHashTable();  // Free the hash table memory 
-    return 0; 
+    if (!partSizes) {
+        perror("Memory allocation for part sizes failed");
+        MPI_Finalize();
+        return 1;
+    }
+
+    char **textParts = NULL;
+    if (rank == 0) {
+        textParts = splitFileToStrings(FILENAME, &numParts, partSizes);
+        if (!textParts) {
+            free(partSizes);
+            MPI_Finalize();
+            return 1;
+        }
+    }
+
+    // Broadcast the number of parts to all processes
+    MPI_Bcast(&numParts, 1, MPI_INT, 0, MPI_COMM_WORLD);
+    MPI_Bcast(partSizes, numParts, MPI_INT, 0, MPI_COMM_WORLD);
+
+    int chunk_size = numParts / size;
+    int start = rank * chunk_size;
+    int end = (rank == size - 1) ? numParts : start + chunk_size;
+
+    // Each process processes its chunk of text
+    for (int i = start; i < end; i++) {
+        char *part;
+        if (rank == 0) {
+            // Master process already has the text parts
+            part = textParts[i];
+        } else {
+            // Receive part size from master
+            int partSize = partSizes[i];
+            part = (char *)malloc((partSize + 1) * sizeof(char));
+            MPI_Recv(part, partSize + 1, MPI_CHAR, 0, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+        }
+
+        // Process the part and free the memory
+        processPart(part);
+        if (rank != 0) {
+            free(part);
+        }
+    }
+
+    if (rank == 0) {
+        for (int i = 0; i < numParts; i++) {
+            free(textParts[i]);
+        }
+        free(textParts);
+    }
+    free(partSizes);
+
+    // Reduction to find the most frequent word across all processes
+    char localMostFreqWord[MAX_WORD_LEN];
+    int localMaxCount;
+    findMostFrequentWord(localMostFreqWord, &localMaxCount);
+
+    char globalMostFreqWord[MAX_WORD_LEN];
+    int globalMaxCount;
+    MPI_Reduce(&localMaxCount, &globalMaxCount, 1, MPI_INT, MPI_MAX, 0, MPI_COMM_WORLD);
+
+    if (rank == 0) {
+        printf("Most frequent word: '%s' (appears %d times)\n", globalMostFreqWord, globalMaxCount);
+    }
+
+    cleanupHashTable();
+    MPI_Finalize();
+    return 0;
 }
