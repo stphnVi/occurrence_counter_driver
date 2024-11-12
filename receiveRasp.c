e5fc772555d4bd828aa547bb68ba8f89f7b17bb6
stphnVi <stephanie1117@estudiantec.cr>
2024-11-12 03:55:01 +0000

    update files org

--- receiveRasp.c
+++ receiveRasp.c
@@ -6,9 +6,11 @@
 #include <termios.h>
 #include <errno.h>
 
-int configurar_uart(const char *puerto_uart) {
+int configurar_uart(const char *puerto_uart)
+{
     int uart_fd = open(puerto_uart, O_RDWR | O_NOCTTY | O_NDELAY);
-    if (uart_fd == -1) {
+    if (uart_fd == -1)
+    {
         perror("Error abriendo el UART");
         return -1;
     }
@@ -30,24 +32,32 @@ int configurar_uart(const char *puerto_uart) {
     return uart_fd;
 }
 
-int main() {
-    const char *puerto = "/dev/serial0";  // Puerto UART en Raspberry Pi
+int main()
+{
+    const char *puerto = "/dev/serial0"; // Puerto UART en Raspberry Pi
     int uart_fd = configurar_uart(puerto);
-    if (uart_fd == -1) {
+    if (uart_fd == -1)
+    {
         return 1;
     }
 
     char buffer[256];
     memset(buffer, 0, sizeof(buffer));
-    int bytes_recibidos = read(uart_fd, buffer, sizeof(buffer) - 1);
-    if (bytes_recibidos < 0) {
-        perror("Error recibiendo mensaje");
-        close(uart_fd);
-        return 1;
-    } else {
-        printf("Mensaje recibido: %s\n", buffer);
-    }
 
+    while (1)
+    {
+        int bytes_recibidos = read(uart_fd, buffer, sizeof(buffer) - 1);
+        if (bytes_recibidos < 0)
+        {
+            perror("Error recibiendo mensaje");
+            close(uart_fd);
+            return 1;
+        }
+        else
+        {
+            printf("Mensaje recibido: %s\n", buffer);
+        }
+    }
     close(uart_fd);
     return 0;
 }
