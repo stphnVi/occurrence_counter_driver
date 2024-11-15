f8b76212d6ae8183ce4eeb3fab0a3c4473c2f20b
valeriam07 <47421582+valeriam07@users.noreply.github.com>
2024-11-15 12:28:09 -0600

    .a library file

--- testLib.c
+++ testLib.c
@@ -6,3 +6,15 @@ int main()
     draw("TOLEBRIO\n");
     return 0;
 }
+
+/**
+ * Consideraciones para utilizar draw():
+ * 1. El string debe ser en letras mayusculas
+ * 2. Debe finalizar con un salto de linea \n
+ * 
+ * Compilar un archivo que utiliza la libreria: gcc program.c -L. -lsendRasp -o program
+ * Esto vincula la libreria (.a) al archivo ejecutable
+ * Hacer include de sendRasp.h
+ *
+
+ */
